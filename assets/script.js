3cc2806662ccb33478037e879ff2b351905e8083
Coral <cnight18@hotmail.com>
2023-05-29 22:16:58 -0300

    css updates , need questions , scoreboard link

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,21 @@
+ //quiz ( add btn listenrs , **note go in order**)
+  const startContainer = document.getElementById("start-container");
+  const quizContainer = document.getElementById("quiz-container");
+  const questionElement = document.getElementById("question");
+  const choicesElement = document.getElementById("choices");
+
+  //scoreboard ( **note localstorage display)
+  const scoreElement = document.getElementById("score");
+  const nameInput = document.getElementById("name");
+  const submitScoreBtn = document.getElementById("submit-score-btn");
+  const scoreboardElement = document.getElementById("scoreboard");
+  const timerElement = document.getElementById("timer");
+
+  let currentQuestion = 0;
+  let score = 0;
+  let timeLeft = 60; // 60 seconds
+  let timer;
+
  // questions 
  const quizData = [
     {
@@ -26,23 +44,6 @@
       correctAnswer: 1,
     },];
 
-  //quiz ( add btn listenrs , **note go in order**)
-  const startContainer = document.getElementById("start-container");
-  const quizContainer = document.getElementById("quiz-container");
-  const questionElement = document.getElementById("question");
-  const choicesElement = document.getElementById("choices");
-
-  //scoreboard ( **note localstorage display)
-  const scoreElement = document.getElementById("score");
-  const nameInput = document.getElementById("name");
-  const submitScoreBtn = document.getElementById("submit-score-btn");
-  const scoreboardElement = document.getElementById("scoreboard");
-  const timerElement = document.getElementById("timer");
-
-  let currentQuestion = 0;
-  let score = 0;
-  let timeLeft = 60; // 60 seconds
-  let timer;
 
  // quiz start and timer start  
   function showStartPage() {
